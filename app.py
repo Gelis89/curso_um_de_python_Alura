5d0c20e38ce89de1dd8c09104fdb08c2eef8e1fc
Gelis <henriquesnogueira2004@gmail.com>
2024-10-21 17:54:52 -0300

    aprendendo try e except

--- app.py
+++ app.py
@@ -22,23 +22,37 @@ def finalizar_app():
     os.system('cls')
     print('encerrando app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opcao: '))
-    print(f'Você escolheu a opção {opcao_escolhida}')
+def opcao_invalida():
+    print('Opcção invalida\n')
+    input('digite um numero para opção principal: ')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('2. Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('3. Ativar restaurante')
-    else:
-        finalizar_app()
+def escolher_opcao():
+    #tentar executar, caso não funcione ele exibe o except
+    try:
+        opcao_escolhida = int(input('Escolha uma opcao: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('2. Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('3. Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
+
 #definindo como aquivo main(principal)
 if __name__ == '__main__':
     main()
